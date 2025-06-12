d521a8a83f2e41ea49352145f1fdca141f2208c5
udekas <marek.udekas@hotmail.com>
2025-06-12 23:41:34 +0300

    Refactor ProductFactory to utilize Faker methods directly and streamline image URL generation

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ namespace Database\Factories;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as FakerFactory;
+
+$faker = FakerFactory::create();
+
 
 /**
  * @extends Factory<Product>
@@ -13,12 +17,13 @@ class ProductFactory extends Factory
     protected $model = Product::class;
 
     public function definition(): array
-    {
-        return [
-            'name' => $this->faker->sentence(2),
-            'description' => $this->faker->sentence(5),
-            'image' => $this->faker->imageUrl(400, 400, 'products', true),
-            'price' => $this->faker->numberBetween(10, 200),
-        ];
-    }
+{
+    return [
+        'name' => $this->faker->sentence(2),
+        'description' => $this->faker->sentence(5),
+        'image' => $this->faker->imageUrl(400, 400),
+        'price' => $this->faker->numberBetween(10, 200),
+    ];
+}
+
 }
