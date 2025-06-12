bcf17c67e6459a803d12a76438f21969e693824f
udekas <marek.udekas@hotmail.com>
2025-06-12 23:19:29 +0300

    Refactor ProductFactory to use built-in Faker methods and remove unused provider

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,27 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Mmo\Faker\PicsumProvider;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ * @extends Factory<Product>
  */
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Product::class;
+
     public function definition(): array
     {
-        fake()->addProvider(PicsumProvider::class);
         return [
-            'name' => fake()->sentence(2),
-            'description' => fake()->sentence(5),
-            'image' => fake()->picsumUrl(400, 400),
-            'price' => fake()->numberBetween(10, 200)
+            'name' => $this->faker->sentence(2),
+            'description' => $this->faker->sentence(5),
+            'image' => $this->faker->imageUrl(400, 400, 'products', true),
+            'price' => $this->faker->numberBetween(10, 200),
         ];
     }
 }
