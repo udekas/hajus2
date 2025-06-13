f229971c6e390ce320ab68e8f5202f55536248bb
udekas <marek.udekas@hotmail.com>
2025-06-13 23:17:36 +0300

    Enhance Checkout page styling and improve form accessibility

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 // Products
-Route::get('/products', [ProductController::class, 'index'])->middleware('auth')->name('products.index');
+Route::get('/products', [ProductController::class, 'index'])->middleware('auth')->name('products.indcex');
 
 // Cart
 Route::controller(CartController::class)
