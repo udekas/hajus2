8c4e9b2b0f7bcda7d2bb991929ffe0ba96f89f4f
udekas <marek.udekas@hotmail.com>
2025-06-16 22:37:41 +0300

    Fix Stripe API key usage in CheckoutController to use public key for session creation

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -31,7 +31,7 @@ class CheckoutController extends Controller
             return redirect()->route('checkout.index')->with('error', 'Your cart is empty.');
         }
 
-        Stripe::setApiKey(config('services.stripe.secret'));
+        Stripe::setApiKey(config('services.stripe.public'));
 
         $lineItems = [];
 
